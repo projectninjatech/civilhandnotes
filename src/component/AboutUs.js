1bad45a516d09a0363938cbcef03d2c4749eab76
ProjectNinjaTech <projectninjatech@gmail.com>
2024-06-24 13:19:21 +0530

    about page

--- src/component/AboutUs.js
+++ src/component/AboutUs.js
@@ -8,7 +8,7 @@ const AboutUs = () => {
       <div className="about-us-content">
         <div className="about-us-text">
           <h1>About Us</h1>
-          <p>We are a small team of two engineers with a shared passion for education and a desire to help GATE aspirants achieve their dreams. Recognizing the challenges students face in preparing for this competitive exam, we decided to create and sell high-quality PDF notes at a low cost. Our notes are designed to be comprehensive, easy to understand, and affordable, making them accessible to everyone.</p>
+          <p>We are a small team of two engineers with a shared passion for education and a desire to help GATE aspirants achieve their dreams. Recognizing the challenges students face in preparing for this GATE competitive exam, we decided to create and sell high-quality PDF notes at a low cost. Our notes are designed to be comprehensive, easy to understand, and affordable, making them accessible to everyone.</p>
         </div>
         <div className="about-us-image">
           <img src={teamImage} alt="Our Team" />
